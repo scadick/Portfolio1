6ae1ff5cea634591fb38197478e789f84fbdb8e9
Steven <s_cadick2@fanshaweonline.ca>
2018-01-10 17:28:19 -0500

    editing

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php
-	require_once('admin/scripts/config.php');
+	require_once('admin/config.php');
 	if(isset($_POST['name'])){
 		//echo "yup";
 		$direct = "thankyou.php";
@@ -18,60 +18,3 @@
 		}*/
 	}
 ?>
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Contact Us</title>
-</head>
-<body>
-    <form class="small-12 cell" id="contactForm" action="contact.php" method="post">
-            		<div class="grid-x">
-                    <div class="small-12 medium-2 medium-offset-2 cell">
-                    <label for="name">First Name</label>
-                    </div>
-                    <div class="small-12 medium-8 cell">
-                    <input type="text" required id="name" name="firstName" size="30" placeholder="Enter your first name">
-                    </div>
-                    </div>
-                    <div class="grid-x">
-                    <div class="small-12 medium-2 medium-offset-2 cell">
-                    <label for="name">Last Name</label>
-                    </div>
-                    <div class="small-12 medium-8 cell">
-                    <input type="text" required id="name" name="lastName" size="30" placeholder="Enter your last name">
-                    </div>
-                    </div>
-                    <div class="grid-x">
-                    <div class="small-12 medium-2 medium-offset-2 cell">
-                    <label for="email">Email Address</label>
-                    </div>
-                    <div class="small-12 medium-8 cell">
-                    <input type="email" required id="email" size="30" placeholder="Enter your Email">
-                    </div>
-                    </div>
-                    <div class="grid-x">
-                    <div class="small-12 medium-2 medium-offset-2 cell">
-                    <label for="tel">Phone Number</label>
-                    </div>
-                    <div class="small-12 medium-8 cell">
-                    <input type="tel" id="phone" size="30" placeholder="Enter your Phone Number">
-                    </div>
-                    </div>
-                    <div class="grid-x">
-                    <label class="small-12 medium-2 medium-offset-2 cell" for="description">Description</label>
-                    <textarea class="small-12 medium-8 cell" name="description" id="description" placeholder="Please enter a description of the work you would like done" required cols="50" rows="8"></textarea>
-                    </div>
-                    <div class="grid-x hide">
-                    	<div class="small-12 cell">
-                        	<input type="street" id="street" size="30" placeholder="Enter your Address">
-                    	</div>
-                    </div>
-                    <div class="grid-x">
-                    <div class="small-12 cell">
-                    <input id="submit" type="submit" value="Subscribe">
-                    </div>
-                    </div>
-            </form>
-</body>
-</html>
